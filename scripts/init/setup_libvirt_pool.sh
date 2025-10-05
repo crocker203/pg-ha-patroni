8addfe0cff72e5f87fdf857f7970389d080854f4
crocker203 <235141069+crocker203@users.noreply.github.com>
2025-10-06 03:21:55 +0700

    Update setup_libvirt_pool.sh

--- scripts/init/setup_libvirt_pool.sh
+++ scripts/init/setup_libvirt_pool.sh
@@ -6,6 +6,14 @@ POOL_PATH="${PG_HA_PATRONI_HOME}/vagrant_images"
 
 mkdir -p "$POOL_PATH"
 
+# Определяем пользователя libvirt-qemu
+LIBVIRT_USER="qemu"   # часто uid:gid = 64055:993, зависит от системы
+LIBVIRT_GROUP="kvm"
+
+# Устанавливаем права для libvirt
+sudo chown -R $LIBVIRT_USER:$LIBVIRT_GROUP "$POOL_PATH"
+sudo chmod -R 770 "$POOL_PATH"
+
 # Проверяем, существует ли уже пул
 if ! virsh pool-info "$POOL_NAME" &>/dev/null; then
     echo "⚙️ Создаём libvirt storage pool $POOL_NAME..."
